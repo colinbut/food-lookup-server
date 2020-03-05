dfbe5a5595c0e5bc474fa0ec0ba3c34284d55471
Colin But <colin.but@outlook.com>
2020-03-05 21:37:43 +0000

    first commit

---
+++ src/main/java/com/mycompany/foodlookup/FoodlookupApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.foodlookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodlookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/foodlookup/FoodlookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.foodlookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
